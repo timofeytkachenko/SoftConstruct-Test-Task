eddcd651b61ded2027a1705dcd8f3fc4b2f04ad4
Timofey Tkachenko <timofey_tkachenko@pm.me>
2024-07-21 20:02:17 +0300

    outliers removing added

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import math
+from collections import Counter
+import weightedstats as ws
 
 
 def plot_feature_distributions(data):
@@ -68,3 +70,117 @@ def create_interactive_pie_charts(df, num_bins=10):
 
         # Show the chart
         fig.show()
+
+
+class Med_couple:
+    def __init__(self, data):
+        self.data = np.sort(data, axis=None)[::-1]  # sorted decreasing
+        self.med = np.median(self.data)
+        self.scale = 2 * np.amax(np.absolute(self.data))
+        self.Zplus = [(x - self.med) / self.scale for x in self.data if x >= self.med]
+        self.Zminus = [(x - self.med) / self.scale for x in self.data if x <= self.med]
+        self.p = len(self.Zplus)
+        self.q = len(self.Zminus)
+
+    def H(self, i, j):
+        a = self.Zplus[i]
+        b = self.Zminus[j]
+
+        if a == b:
+            return np.sign(self.p - 1 - i - j)
+        else:
+            return (a + b) / (a - b)
+
+    def greater_h(self, u):
+
+        P = [0] * self.p
+
+        j = 0
+
+        for i in range(self.p - 1, -1, -1):
+            while j < self.q and self.H(i, j) > u:
+                j += 1
+            P[i] = j - 1
+        return P
+
+    def less_h(self, u):
+
+        Q = [0] * self.p
+
+        j = self.q - 1
+
+        for i in range(self.p):
+            while j >= 0 and self.H(i, j) < u:
+                j = j - 1
+            Q[i] = j + 1
+
+        return Q
+
+    # Kth pair algorithm (Johnson & Mizoguchi)
+    def kth_pair_algorithm(self):
+        L = [0] * self.p
+        R = [self.q - 1] * self.p
+
+        Ltotal = 0
+
+        Rtotal = self.p * self.q
+
+        medcouple_index = math.floor(Rtotal / 2)
+
+        while Rtotal - Ltotal > self.p:
+
+            middle_idx = [i for i in range(self.p) if L[i] <= R[i]]
+            row_medians = [self.H(i, math.floor((L[i] + R[i]) / 2)) for i in middle_idx]
+
+            weight = [R[i] - L[i] + 1 for i in middle_idx]
+
+            WM = ws.weighted_median(row_medians, weights=weight)
+
+            P = self.greater_h(WM)
+
+            Q = self.less_h(WM)
+
+            Ptotal = np.sum(P) + len(P)
+            Qtotal = np.sum(Q)
+
+            if medcouple_index <= Ptotal - 1:
+                R = P
+                Rtotal = Ptotal
+            else:
+                if medcouple_index > Qtotal - 1:
+                    L = Q
+                    Ltotal = Qtotal
+                else:
+                    return WM
+        remaining = np.array([])
+
+        for i in range(self.p):
+            for j in range(L[i], R[i] + 1):
+                remaining = np.append(remaining, self.H(i, j))
+
+        find_index = medcouple_index - Ltotal
+
+        k_minimum_element = remaining[np.argpartition(remaining, find_index)]  # K-element algothrm
+
+        return k_minimum_element[find_index]
+
+
+def detection_outlier(n, df):
+    outlier_indices = []
+
+    for col in df.columns:
+        Q1 = np.percentile(df[col], 25)
+        Q3 = np.percentile(df[col], 75)
+        IQR = Q3 - Q1
+        outlier_step = 1.5 * IQR
+        medcouple = Med_couple(np.array(df[col])).kth_pair_algorithm()
+        if medcouple >= 0:
+            outlier_list_col = df[(df[col] < Q1 - outlier_step * math.exp(-3.5 * medcouple)) | (
+                        df[col] > Q3 + outlier_step * math.exp(4 * medcouple))].index
+        else:
+            outlier_list_col = df[(df[col] < Q1 - outlier_step * math.exp(-4 * medcouple)) | (
+                        df[col] > Q3 + outlier_step * math.exp(3.5 * medcouple))].index
+        outlier_indices.extend(outlier_list_col)
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list(k for k, v in outlier_indices.items() if v > n)
+    return multiple_outliers
